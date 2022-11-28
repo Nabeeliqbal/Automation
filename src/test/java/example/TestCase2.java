a4849bb9df4b32c9875149203627dc56418c9e26
nabeeliqbal <nabeeliqbal399@gmail.com>
2022-11-28 12:17:46 +0500

    Test Case 2

--- src/test/java/example/TestCase2.java
+++ src/test/java/example/TestCase2.java
@@ -7,11 +7,10 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.interactions.Actions;
 
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
+
 import org.testng.annotations.Test;
 
-import java.time.Duration;
+
 
 public class TestCase2 {
     @org.junit.Test
